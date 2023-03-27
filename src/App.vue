13bb1d8164cf3f8ac232220a5685ca3991cd3e4c
konkarin <konkarin.photo@gmail.com>
2023-03-27 23:42:47 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView v-slot="{ Component }">
+    <Suspense timeout="0">
+      <template #default>
+        <div>
+          <component :is="Component" />
+        </div>
+      </template>
+      <template #fallback>
+        <div>Loading...</div>
+      </template>
+    </Suspense>
+  </RouterView>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const message = ref('')
+
+message.value = await new Promise((resolve) => {
+  setTimeout(() => {
+    resolve('ok')
+  }, 2000)
+})
+</script>
+
+<template>
+  <div>{{ message }}</div>
+</template>
